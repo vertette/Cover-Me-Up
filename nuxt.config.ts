89a201f1e660117384acd8a61cbc36769df8fdc4
vertette <vertettegd@gmail.com>
2025-12-13 23:56:11 +0100

    Add: GitHub workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
       title: 'Cover Me Up',
       charset: 'utf-8',
     },
+    baseURL: '/Cover-Me-Up/',
   },
   css: ['~/assets/css/tailwind.css'],
   compatibilityDate: '2024-11-01',
